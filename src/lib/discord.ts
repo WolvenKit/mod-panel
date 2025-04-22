1464d23fad0c79e19fa5528df8441b732d7a04a8
Moonded <moonded@moonded.com>
2025-04-22 12:48:43 +0200

    update fetchuser

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,18 +1,42 @@
 export interface DiscordUser {
   id: string;
   username: string;
+  avatar: string;
+  discriminator: string;
+  public_flags: number;
+  flags: number;
+  banner: string | null;
+  accent_color: number | null;
   global_name?: string;
+  avatar_decoration?: {
+    asset: string;
+    sku_id: string;
+    expires_at: string | null;
+  };
+  collectibles: string | null;
+  banner_color: string | null;
+  clan: string | null;
+  primary_guild: string | null;
+  mfa_enabled: boolean;
+  locale: string;
+  premium_type: number;
+  email: string | null;
+  verified: boolean;
 }
 
-export async function fetchCurrentUser(): Promise<DiscordUser | undefined> {
-  const response = await fetch(`https://discord.com/api/v10/users/@me`, {
-    mode: "cors",
-    credentials: "include",
-  });
-
-  if (!response.ok) {
-    return;
+export async function fetchCurrentUser(): Promise<DiscordUser | null> {
+  function getCookie(name: string) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+      const part = parts.pop();
+      return part ? part.split(";").shift() : undefined;
+    }
   }
 
-  return await response.json();
+  const match = getCookie("discord");
+
+  if (!match) return null;
+  console.log(decodeURIComponent(match))
+  return JSON.parse(decodeURIComponent(match)) as DiscordUser;
 }
